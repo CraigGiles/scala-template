b725ff44dc3c56128fa099f0c971ec78c0cfa919
Craig Giles <craig@gilesc.com>
2016-10-11 06:06:18 -0700

    Added flyway

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,5 +4,6 @@ import Keys._
 object Dependencies {
   def cats = "org.typelevel" %% "cats" % "0.7.2"
   def scalatest(scope: String) = "org.scalatest" %% "scalatest" % "2.2.6" % scope
+  def flywayCore = "org.flywaydb" % "flyway-core" % "4.0"
   def mysqlConnectorJava = "mysql" % "mysql-connector-java" % "5.1.36"
 }
